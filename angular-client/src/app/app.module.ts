cec6d8d5591a3b4dce66b0fd6176c37f89f51f40
Alex Alsheuski <alex@alsheuski.com>
2017-09-15 10:16:03 +0300

    Add angular animations, material, icons and flex layout

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, FlexLayoutModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
